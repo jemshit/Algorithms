6d5251dcf47729e24cdd16d39f355fdfc8d27db2
Jemshit Iskenderov <jemshit@mail.com>
2019-11-03 16:46:47 +0300

    Dijkstra Shortest Path

---
+++ src/main/kotlin/graph/ShortestPathDijkstra.kt
@@ -0,0 +1,38 @@
+package graph
+
+import java.util.*
+
+private data class NodeDistancePair(val node: Int, val distance: Int) : Comparable<NodeDistancePair> {
+    override fun compareTo(other: NodeDistancePair): Int {
+        return distance.compareTo(other.distance)
+    }
+}
+
+fun GraphUsingAdjacencyList.shortestPathDijkstra(): Array<Int> {
+    val shortestPaths = arrayOfNulls<Int>(nodeCount)
+    shortestPaths[0] = 0
+    val priorityQueue = PriorityQueue<NodeDistancePair>() // node, distance
+    priorityQueue.add(NodeDistancePair(0, 0))
+    val visited: Array<Boolean> = Array<Boolean>(nodeCount) { false }
+
+    while (priorityQueue.isNotEmpty()) {
+        val (node, distance) = priorityQueue.poll()
+        visited[node] = true
+
+        if (shortestPaths[node] != null && shortestPaths[node]!! < distance)
+            continue
+        if (shortestPaths[node] == null || shortestPaths[node]!! > distance)
+            shortestPaths[node] = distance
+
+        for (edge in getEdges(node)) {
+            val neighbour = edge.end
+            // You can't get shorter path if it is visited before
+            if (!visited[neighbour]) {
+                val neighbourDistance = shortestPaths[node]!! + (edge.weight ?: 0)
+                shortestPaths[neighbour] = neighbourDistance
+                priorityQueue.add(NodeDistancePair(neighbour, neighbourDistance))
+            }
+        }
+    }
+    return shortestPaths as Array<Int>
+}
